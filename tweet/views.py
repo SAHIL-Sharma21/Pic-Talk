57d476488d59ae6cdf251ffafd094221d03d195b
SAHIL-Sharma21 <sahilsharma2000dtc@gmail.com>
2024-07-28 22:30:35 +0530

    writng views for create tweet, get alltweets

--- tweet/views.py
+++ tweet/views.py
@@ -1,7 +1,35 @@
 from django.shortcuts import render
+from .models import Tweet
+from .forms import TweetForm
+from django.shortcuts import get_object_or_404, redirect #like ORM which used to talk to database
 
 # Create your views here.
 #testing view
 
 def index(request):
     return render(request, 'index.html')
+
+
+#list all tweets
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request, 'tweet_list.html', {'tweets': tweets})
+
+
+#create tweets
+def create_tweet(request):
+    if request.method == "POST":
+        form = TweetForm(request.POST, request.FILES) #handling form/ making form -> Django superpower
+        if form.is_valid():
+            tweet = form.save(commit=False) #commit = fasle means db mei store nhi krenge bs form ka data save rahega
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm()
+    return render(request, 'tweet_form.html', {'form': form})
+
+
+#edit tweet / form
+def tweet_edit(request):
+    pass
