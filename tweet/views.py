15ba24356f2ece139d6093f0af255f7eaffaea4c
SAHIL-Sharma21 <sahilsharma2000dtc@gmail.com>
2024-07-29 20:01:43 +0530

    all views logic complete -> CRUD of tweet

--- tweet/views.py
+++ tweet/views.py
@@ -31,5 +31,24 @@ def create_tweet(request):
 
 
 #edit tweet / form
-def tweet_edit(request):
-    pass
+def tweet_edit(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user) #user is forn the authorized user who owns that particul;ar tweet, he canot delete othwer people tweer
+    if request.method == "POST":
+        form = TweetForm(request.POST, request.FILES, instance=Tweet) #instance chaiye rahega -> purani values chaiye rahegi tabhi pta chalega ki purana tweet edit krna hai
+        if form.is_valid():
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            form.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm(instance=tweet)
+    return render(request, 'tweet_form.html', {'form': form})
+
+#tweet delete
+def tweet_delete(request, tweet_id):
+    tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
+    if request.method == "POST":
+        tweet.delete()
+        return redirect('tweet_list')
+    return render(request, 'tweet_confirm_delete.html', {'tweet': tweet})
+ 
