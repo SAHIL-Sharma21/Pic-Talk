8987ca3c11300b35aec558ba52ebf85979cb2595
SAHIL-Sharma21 <sahilsharma2000dtc@gmail.com>
2024-07-30 22:42:00 +0530

    added home route

--- pic_talk/urls.py
+++ pic_talk/urls.py
@@ -19,10 +19,12 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth.urls import views as auth_views
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tweet/', include('tweet.urls')),
-    path('accounts/', include('django.contrib.auth.urls'))
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', views.home_page, name="home_page")
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #adding configuration in urls is to serve media files
  
